d8d2faccc00db467d6077ea435402b928ceb48ed
Anthony Stephen Akinsida <45897960+Dominion2068@users.noreply.github.com>
2022-03-11 20:20:41 +0100

    Add files via upload

--- shop.py
+++ shop.py
@@ -15,14 +15,14 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-# hide_st_style = """
-#             <style>
-#             #MainMenu {visibility: hidden;}
-#             footer {visibility: hidden;}
-#             header {visibility: hidden;}
-#             </style>
-#             """
-# st.markdown(hide_st_style, unsafe_allow_html=True)
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
 # padding = 0
